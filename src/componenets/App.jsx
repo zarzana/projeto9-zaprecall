9e7ea95fba732aae77d9171e86a37a0c64a6dc83
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-05-19 16:11:20 -0300

    Basic setup of HTML and global styles.

---
+++ src/componenets/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+      <p>test</p>
+  )
+  
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './componenets/App.jsx'
+import { GlobalStyle, ResetStyle } from './theme/globalStyles.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ResetStyle />
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
 )
