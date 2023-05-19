8223ae6029f07591c7ad70087699d7a2cf45aa85
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-05-19 19:31:42 -0300

    Implemented opened card state.

--- src/componenets/Card.jsx
+++ src/componenets/Card.jsx
@@ -1,54 +1,81 @@
+import { useState } from "react";
 import styled from "styled-components";
-import playArrow from "../assets/seta_play.png";
+import playButton from "../assets/seta_play.png";
+import flipButton from "../assets/seta_virar.png";
 
 function Card(props) {
 
-  const CardSyle = styled.div`
+  const cardNumber = props.index + 1;
+  const question = props.question;
+  const answer = props.answer;
+
+  const [cardStatus, setCardStatus] = useState('closed');
+  const [cardText, setCardText] = useState(cardNumber);
+
+  const CardStyle = styled.div`
 
   width: 300px;
-  height: 65px;
+  height: ${['closed', 'finished'].includes(cardStatus) ? '65px' : '130px'};
 
-  background: #FFFFFF;
+  background: ${['closed', 'finished'].includes(cardStatus) ? '#FFFFFF' : '#FFFFD5'};
   box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
   border-radius: 5px;
 
   margin-bottom: 25px;
 
   div {
+
+    position: relative;
     height: 65px;
     margin-left: 15px;
     margin-right: 15px;
     display: flex;
     justify-content: space-between;
-    align-items: center;
-  }
+    align-items: ${['closed', 'finished'].includes(cardStatus) ? 'center' : 'none'};
+    padding-top: ${['closed', 'finished'].includes(cardStatus) ? '0' : '18px'};
 
-  h2 {
-    font-family: 'Recursive';
-    font-weight: 700;
-    font-size: 16px;
-    line-height: 19px;
-    color: #333333;
-  }
+    h2 {
+      font-family: 'Recursive';
+      font-weight: ${['closed', 'finished'].includes(cardStatus) ? '700' : '400'};
+      font-size: ${['closed', 'finished'].includes(cardStatus) ? '16px' : '18px'};
+      line-height: ${['closed', 'finished'].includes(cardStatus) ? '19px' : '22px'};
+      color: #333333;
+    }
 
-  img {
-    width: 20px;
-    height: 23px;
+    #playButton {
+      display: ${['closed'].includes(cardStatus) ? 'block' : 'none'};
+      width: 20px;
+      height: 23px;
+    }
+
+    #flipButton {
+      display: ${['opened'].includes(cardStatus) ? 'block' : 'none'};
+      position: absolute;
+      width: 30px;
+      height: 20px;
+      right: 0;
+      margin-top: 82px;
+    }
+  
   }
 
 `;
 
-  const cardNumber = props.index + 1;
-  const question = props.question;
-  const answer = props.answer;
+  const cardClick = () => {
+
+    setCardStatus('opened');
+    setCardText(question);
+
+  };
 
   return (
-    <CardSyle data-test="flashcard">
+    <CardStyle data-test="flashcard">
       <div>
-        <h2 data-test="flashcard-text">Pergunta {cardNumber}</h2>
-        <img src={playArrow} data-test="play-btn"></img>
+        <h2 data-test="flashcard-text">Pergunta {cardText}</h2>
+        <img id='playButton' src={playButton} onClick={cardClick} data-test="play-btn"></img>
+        <img id='flipButton' src={flipButton} data-test="turn-btn"></img>
       </div>
-    </CardSyle>
+    </CardStyle>
   )
 
 }
